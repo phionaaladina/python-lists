979cc2da5b43076c719843d395cb55f4ba3358c9
Phiona Aladina <91528734+phionaaladina@users.noreply.github.com>
2024-01-27 11:30:19 +0300

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,22 @@
+my_string = 'HELLO PYTHON'
+
+print(my_string[0])
+
+
+print(my_string[-1])
+
+
+print(my_string[1])
+
+
+print(my_string[0:5])
+
+print(my_string[6:12])
+
+print(my_string[-4])
+
+print(my_string[:6])
+
+print(my_string[1:-1]) 
+
+#formatted strings
